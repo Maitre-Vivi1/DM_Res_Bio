c34f1baf3d6d3e583acc2fa6b22c03da92aaf3be
Maitre_Vivi1 <viviendupont21@gmail.com>
2021-09-23 15:48:12 +0200

    Ajout question 2.1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# Travail avec un fichier tabulé / DataFrame
+import pandas as pd
+# Pour l'écriture du graph d'intéractions
+import json
+
 # Question 1.1
 
 Graph_Inter_Vivien = {
@@ -13,29 +18,37 @@ Graph_Inter_Vivien = {
     "J": ["G", "H", "I"]
 }
 
-import json
-
 with open('Graph_Inter_Vivien.txt', 'w') as file:
     file.write(json.dumps(Graph_Inter_Vivien))
 
-
 # Question 2.1
 
-# Travail avec un fichier tabulé / DataFrame
-import pandas as pd
+# On charge le fichier correspondant au travail demandé dans l'environnement de travail en précisant
+# le nom des deux colonnes
+Human_HighQuality = pd.read_csv('Human_HighQuality.txt', sep='\t', header=0, names=['Sommet', 'Interaction'])
+donnees_test = Human_HighQuality.loc[1:50]
 
-# On charge le fichier correspondant au travail demandé dans l'environnement de travail en précisant le nom des deux colonnes
-Human_HighQuality = pd.read_csv('Human_HighQuality.txt', sep='\t', header=0, names=['Sommet','Interaction'])
+
+# fonction utilisée afin de délister les éléments
+def flatten(d):
+ v = [[i] if not isinstance(i, list) else flatten(i) for i in d]
+ return [i for b in v for i in b]
+
 
 # Construction de la fonction permettant de décrire les intéractions entre protéines
 def read_interaction_file_dict(nom_fichier):
-    dictionnaire = {}
-    for i in range(1,len(nom_fichier)):
+    dico = {} # Initialisation du dictionnaire
+    for a in range(1, len(nom_fichier)):
+        try:
+            if len(dico[nom_fichier.Sommet[a]]) > 0:
+                dico[nom_fichier.Sommet[a]] = flatten([dico[nom_fichier.Sommet[a]], nom_fichier.Interaction[a]])
+                pass
+        except:
+            dico[nom_fichier.Sommet[a]] = nom_fichier.Interaction[a]
+    return dico
 
-        while nom_fichier.at[i-1, "Sommet"] == nom_fichier.at[i, "Sommet"]:
-            inter = [inter, nom_fichier.at[i-1, "Sommet"]]
 
-    return dictionnaire
+read_interaction_file_dict(Human_HighQuality)
 
-read_interaction_file_dict(data)
+
 
