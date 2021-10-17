69cad8492b376d62192b2a266d06d73503a896df
vivi1 <viviendupont21@gmail.com>
2021-10-17 17:47:49 +0200

    TU + tout m'a l'air bon

--- Partie_3/tests_unitaires_Chapitre3.py
+++ Partie_3/tests_unitaires_Chapitre3.py
@@ -18,3 +18,14 @@ class TestInteractome(unittest.TestCase, Interactome):
     def test_clustering(self):
         self.assertGreaterEqual(self.df.clustering("ZN384_HUMAN"), 0, "Le coefficient doit être positive")
         self.assertEqual(self.df.clustering("ZYX_HUMAN"), 2/6, 'la densité calculée est fausse')
+
+    def test_count_cc(self):
+        self.assertEqual(self.df.count_cc()[0], 3, "Pas le bon nbre de classes connexes")
+    
+    def test_compute_cc(self):
+        self.assertEqual(len(self.df.compute_cc()), 9, "Tous les sommets sont représentés")
+        
+    def test_extract_cc(self):
+        self.assertEqual(self.df.extract_cc("ZW10_HUMAN"),
+                         ['1433B_HUMAN', 'BAD_HUMAN', 'NEBL_HUMAN', 'ZN384_HUMAN', 'ZYX_HUMAN'],
+                         "Bonne classe convexe")
